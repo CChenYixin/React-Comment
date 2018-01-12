1af94af57fcfd00866c2ffdf578a72500edbaa68
chen <onepiececee@163.com>
2018-01-12 16:33:38 +0800

    划分组件实现组件结构

---
+++ src/CommentInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CommentInput extends Component{
+    render(){
+        return (
+            <div> CommentList </div>
+        )
+    }
+}
+
+export default CommentInput;
